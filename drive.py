29a701d4c0b4937aa85659821b3d3d9ce59cafa3
daktal7 <dakotaflanary@gmail.com>
2019-10-09 19:19:15 -0600

    added my own drive code

---
+++ drive.py
@@ -0,0 +1,27 @@
+from car_control import steer, drive
+from matplotlib import pyplot as plt
+# from gluoncv import model_zoo, utils
+# import pyrealsense2 as rs
+# from PIL import Image
+from signal import signal, SIGINT
+from sys import exit
+import numpy as np
+import mxnet as mx
+import argparse
+import imutils
+import serial
+import time
+# import cv2
+import os
+import gc
+
+# initialize communication with the arduino
+ser = serial.Serial("/dev/ttyUSB0",115200)
+ser.flushInput()
+time.sleep(2)
+
+signal(SIGINT, handler)
+print('Running. Press CTRL-C to exit')
+
+steer(0)
+drive(1500)
