85415f1ba47c006d234811a6d703b537eadd6be5
ShakeJunt1 <jacerozsa@Jaces-MacBook-Pro.local>
2019-10-31 13:16:01 -0600

    added intersections

--- car_ros/src/drive_control/src/wpManager.py
+++ car_ros/src/drive_control/src/wpManager.py
@@ -7,7 +7,22 @@ import matplotlib.image as mpimg
 import math
 import matplotlib.path as mpltPath
 
+#this are the coordinates for all the intersections
+INT_1 = np.array([[878, 454],[822, 395],[970, 259],[1023, 328]])
+INT_2 = np.array([[ 834,  541],[ 952,  671],[ 899,  742],[ 773,  610]])
+INT_3 = np.array([[ 663,  553],[ 527,  677],[ 474,  602],[ 604,  492]])
+INT_4 = np.array([[ 647,  369],[ 700,  330],[ 590,  214],[ 545,  269]])
+INT_5 = np.array([[ 966,  263],[ 909,  210],[ 775,  338],[ 818,  399]])
+INT_6 = np.array([[ 897,  490],[ 834,  543],[ 952,  681],[1015,  626]])
+INT_7 = np.array([[ 722,  624],[ 596,  765],[ 523,  683],[ 657,  555]])
+INT_8 = np.array([[ 594,  430],[ 645,  371],[ 541,  275],[ 489,  340]])
+INT_9 = np.array([[   3,  310],[   5,  448],[  64,  462],[  64,  300]])
+INT_10 = np.array([[ 160,  202],[ 162,  271],[ 286,  275],[ 292,  206]])
+INT_11 = np.array([[1404,  868],[1256,  870],[1260,  791],[1400,  787]])
+INT_12 = np.array([[1569,  756],[1569,  620],[1504,  620],[1506,  761]])
+INTERSECTIONS = np.array([INT_1,INT_2,INT_3,INT_4,INT_5,INT_6,INT_7,INT_8,INT_9,INT_10,INT_11,INT_12])
 
+#Distance that the car must be from a waypoint in order bump that waypoint off our list.
 MIN_DIST = 80
 X = 0
 Y = 1
@@ -88,10 +103,11 @@ def reachedWP(curLoc, wp):
 
 #this function returns the index of the intersection we are in, or returns -1 if we aren't in any intersection
 #An intersection is a 2D array or list with four vertices, so this function needs to be a list of 2D arrays or lists
+#curLoc is a tuple
 def reachedIntersection(curLoc, intersections):
     for i in range(len(intersections)):
         path = mpltPath.Path(intersections[i])
         if path.contains_point(curLoc):
-            return i
+            return i+1
     return -1
 
