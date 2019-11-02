f1868254d34ebb5be42bbcbe0a3a3047c81562eb
zjwalker9 <zjwalker9@gmail.com>
2019-11-01 18:12:28 -0600

    Update Python_MainV4.py

--- noRos/Python_MainV4.py
+++ noRos/Python_MainV4.py
@@ -31,7 +31,7 @@ init_command = "!start1650\n"
 print("4")
 ser.write(init_command.encode())
 print("5")
-init_command = "!inits0.6\n"
+init_command = "!inits0.2\n"
 print("6")
 ser.write(init_command.encode())
 print("7")
@@ -108,7 +108,7 @@ def straight_through_intersection():
 
 
 FRAMES_TO_AVERAGE = 5
-DRIVING_SPEED = 0.1
+DRIVING_SPEED = 0.2
 
 number_of_slices = 5
 dynamic_coordinates_right = [[[123,123],[123,123],[123,123],[123,123]]]
