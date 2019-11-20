566d10bba653587cf0c0037bc35b6b871001d72f
zjwalker9 <zjwalker9@gmail.com>
2019-11-20 12:41:56 -0700

    Update lane_detector.py

--- car_ros/src/drive_control/src/lane_detector.py
+++ car_ros/src/drive_control/src/lane_detector.py
@@ -133,6 +133,8 @@ class image_displayer:
 			# 							   (640, 480), True)
 
 			# write the output frame to disk
+			test_image = RI.filter_yellow(image)
+
 			test_image = cv2.cvtColor(test_image, cv2.COLOR_GRAY2RGB)
 			test_image_resized = cv2.resize(test_image, (640, 480))
 			self.writer.write(test_image)
