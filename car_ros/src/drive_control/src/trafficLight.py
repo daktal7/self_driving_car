6f8eb3581299a03fc264d249f0a6955a24779eae
jaceRozsa <jacerozsa@Jaces-MBP.app.byu.edu>
2019-11-25 10:52:39 -0700

    udpated trafficLight.py

--- car_ros/src/drive_control/src/trafficLight.py
+++ car_ros/src/drive_control/src/trafficLight.py
@@ -15,14 +15,27 @@ SAT_CUTOFF = 150
 
 #box is given as two vertices. It is an array of form [x1, y1, x2, y2]
 #first point must be upper left had corner and second point is lower right
-#the image is given in BGR
+# color format tells if you're going from RGB to HSV or BGR to HSV. Just use the opencv constant used in cvtColor
 #returns 'r' for red and 'g' for green
-
-#commented out stuff is for debugging
+#if box is none the algorithm will run over the entire image
+#commented out stuff used for debugging
 def detectLight(im,box,colorFormat):
     resRed = 0
     resGreen = 0
     #light = np.zeros((box[2]-box[0],box[3]-box[1],3),np.uint16)
+    if box == None:
+        for i in range(len(im[:,0])):
+            for j in range(len(im[0,:])):
+                hsvPix = cv2.cvtColor(np.array([[im[i, j]]]), colorFormat)#YOU MAY NEED TO SWITCH THE J AND THE I
+                if RED_LOWER <= (hsvPix[0, 0, 0] + 20) % 180 <= RED_UPPER and hsvPix[0, 0, 1] > SAT_CUTOFF:
+                    resRed = resRed + 1
+                    # light[i - box[0], j - box[1],0] = 255
+                if GREEN_LOWER <= hsvPix[0, 0, 0] <= GREEN_UPPER and hsvPix[0, 0, 1] > SAT_CUTOFF:
+                    resGreen = resGreen + 1
+                    # light[i - box[0], j - box[1], 1] = 255
+        if resGreen > resRed:
+            return 'g'
+        return 'r'
     for i in range(box[0],box[2]):
         for j in range(box[1],box[3]):
             hsvPix = cv2.cvtColor(np.array([[im[j, i]]]),colorFormat) #YOU MAY NEED TO SWITCH THE J AND THE I
