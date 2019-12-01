eaf4f8c53837b8a6d8840c5f2858acf04ed66d3e
daktal7 <dakotaflanary@gmail.com>
2019-11-30 21:46:30 -0700

    foo

--- car_ros/src/drive_control/src/gpsPublisher.py
+++ car_ros/src/drive_control/src/gpsPublisher.py
@@ -34,7 +34,7 @@ def publishIntersection():
 				turn = ils.useLaneNumber(inter) #check to see if we are going to turn or not
 				if turn is not None:
 					pub.publish(4)
-					f = open("~/Desktop/intersection.txt", "w")
+					f = open("/home/nvidia/Desktop/intersection.txt", "w")
 					f.write("4")
 					f.close()
 					rateInner = rospy.Rate(10)
@@ -44,12 +44,12 @@ def publishIntersection():
 						rateInner.sleep()
 					print("publishing turn %d", turn)
 					pub.publish(2)
-					f = open("~/Desktop/intersection.txt", "w")
+					f = open("/home/nvidia/Desktop/intersection.txt", "w")
 					f.write("2")
 					f.close()
 					time.sleep(1.5)
 					pub.publish(turn)
-					f = open("~/Desktop/intersection.txt", "w")
+					f = open("/home/nvidia/Desktop/intersection.txt", "w")
 					f.write(turn)
 					f.close()
 			#print("found intersection. Intersection value: ", inter)
