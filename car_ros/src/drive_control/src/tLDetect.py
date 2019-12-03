43aa3e1fb7bffc70cd244de0d1f69d470ef14f73
daktal7 <dakotaflanary@gmail.com>
2019-12-02 22:33:05 -0700

    foo

--- car_ros/src/drive_control/src/tLDetect.py
+++ car_ros/src/drive_control/src/tLDetect.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import rospy 
+import time
+from std_msgs.msg import Bool, Int32
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 ROI = 170/613;
 BLACK_THRESH = 20
@@ -15,78 +22,115 @@ GREEN_UPPER = GREEN_CENTER+GREEN_BUFFER+10
 SAT_CUTOFF = 150
 GREEN_CUTOFF = 100
 
-# im must be in hsv
-#returns 'r' for red and 'g' for green
-#if box is none the algorithm will run over the entire image
-#commented out stuff used for debugging
-def isGreen(im):
-    resGreen = 0
-    #light = np.zeros((box[2]-box[0],box[3]-box[1],3),np.uint16)
-    for i in range(len(im[:,0])):
-        for j in range(len(im[0,:])):
-            #hsvPix = cv2.cvtColor(np.array([[im[i, j]]]), colorFormat)#YOU MAY NEED TO SWITCH THE J AND THE I
-            if GREEN_LOWER <= im[i, j, 0] <= GREEN_UPPER and im[i, j, 1] > SAT_CUTOFF:
-                resGreen = resGreen + 1
-                # light[i - box[0], j - box[1], 1] = 255
-    print(resGreen)
-    if resGreen > GREEN_CUTOFF:
-        return True
-    return False
+class tlDetector:
+    def __init__(self):
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("video_topic", Image, self.light_detect)
+        self.intersection_sub = rospy.Subscriber("intersection", Int32, intersect)
+        self.light_pub = rospy.Publisher('light', Bool, queue_size = 1)
+        self.intersection = False
+    # im must be in hsv
+    #returns 'r' for red and 'g' for green
+    #if box is none the algorithm will run over the entire image
+    #commented out stuff used for debugging
+    def isGreen(self,im):
+        resGreen = 0
+        #light = np.zeros((box[2]-box[0],box[3]-box[1],3),np.uint16)
+        for i in range(len(im[:,0])):
+            for j in range(len(im[0,:])):
+                #hsvPix = cv2.cvtColor(np.array([[im[i, j]]]), colorFormat)#YOU MAY NEED TO SWITCH THE J AND THE I
+                if GREEN_LOWER <= im[i, j, 0] <= GREEN_UPPER and im[i, j, 1] > SAT_CUTOFF:
+                    resGreen = resGreen + 1
+                    # light[i - box[0], j - box[1], 1] = 255
+        print(resGreen)
+        if resGreen > GREEN_CUTOFF:
+            return True
+        return False
 
-def getNumBlack(im):
-    res = 0
-    for i in range(len(im[:,0])):
-        for j in range(len(im[0,:])):
-            if im[i,j] < BLACK_THRESH:
-                res = res+1
-    return res
+    def getNumBlack(self,im):
+        res = 0
+        for i in range(len(im[:,0])):
+            for j in range(len(im[0,:])):
+                if im[i,j] < BLACK_THRESH:
+                    res = res+1
+        return res
 
-def getBox(im,bins,maxI,dx):
-    iRight = 0
-    iLeft = 0
-    if maxI < len(bins[:,0]):
-        for i in range(maxI+1,len(bins)):
-            if bins[i,1] > BIN_THRESH*bins[maxI,1]:
-                iRight = iRight + 1
-            else:
-                break
-    if maxI > 0:
-        for i in range(maxI-1,-1,-1):
-            if bins[i,1] > BIN_THRESH*bins[maxI,1]:
-                iLeft = iLeft + 1
-            else:
-                break
-    return im[0:int(ROI*len(hsvIm[:,0])),(maxI-iLeft)*dx:(maxI+iRight)*dx,:]
+    def getBox(self,im,bins,maxI,dx):
+        iRight = 0
+        iLeft = 0
+        if maxI < len(bins[:,0]):
+            for i in range(maxI+1,len(bins)):
+                if bins[i,1] > BIN_THRESH*bins[maxI,1]:
+                    iRight = iRight + 1
+                else:
+                    break
+        if maxI > 0:
+            for i in range(maxI-1,-1,-1):
+                if bins[i,1] > BIN_THRESH*bins[maxI,1]:
+                    iLeft = iLeft + 1
+                else:
+                    break
+        return im[0:int(ROI*len(hsvIm[:,0])),(maxI-iLeft)*dx:(maxI+iRight)*dx,:]
 
-#im must be hsv, res is how many cross sections are made
-#will return numpy array that contains just the traffic light
-def getTL(im):
-    bins = np.zeros((RES,2))
-    dx = len(im[0,:])//RES
-    maxVal = 0
-    maxI = -1
-    for i in range(RES):
-        bins[i,0] = i
-        bins[i,1] = getNumBlack(im[0:int(ROI*len(hsvIm[:,0])),i*dx:i*dx+dx,2])
-        if bins[i,1] > maxVal:
-            maxVal = bins[i,1]
-            maxI = i
-    return getBox(im,bins,maxI,dx)
+    #im must be hsv, res is how many cross sections are made
+    #will return numpy array that contains just the traffic light
+    def getTL(self, im):
+        bins = np.zeros((RES,2))
+        dx = len(im[0,:])//RES
+        maxVal = 0
+        maxI = -1
+        for i in range(RES):
+            bins[i,0] = i
+            bins[i,1] = getNumBlack(im[0:int(ROI*len(hsvIm[:,0])),i*dx:i*dx+dx,2])
+            if bins[i,1] > maxVal:
+                maxVal = bins[i,1]
+                maxI = i
+        return getBox(im,bins,maxI,dx)
 
+    def intersect(self, data):
+        if data.data == 4:
+            self.intersection = True
+            time.sleep(10)
+            self.intersection = False
+        
 
-im = mpimg.imread("images/leehi_tl1.jpg")
+    def light_detect(self, data):
+        if self.intersection == False:
+            pass
+        im = self.bridge.imgmsg_to_cv2(data,"rgb8")
+        if im is None:
+            pass
+        try:
+            hsvIm = cv2.cvtColor(im,cv2.COLOR_RGB2HSV)
+        except:
+            print("Failed to convert to hsv")
+        if hsvIm is None:
+            pass
+        try:
+            light = self.getTL(hsvIm)
+        except:
+            print("failed to get TL")
+        # print(light.length())
+        try:
+            self.light_pub.publish(self.isGreen(light))
+        except:
+            print("Failed to check for green")
+        # roi = 170/len(hsvIm[:,0])
 
-hsvIm = cv2.cvtColor(im,cv2.COLOR_RGB2HSV)
+        # cv2.imshow("Traffic Light", im)
+        # key = cv2.waitKey(1) & 0xFF
 
-light = getTL(hsvIm)
-print(isGreen(light))
-roi = 170/len(hsvIm[:,0])
+        # if key == ord("q"):
+        #     break
+        # plt.figure()
 
-plt.figure()
+        # plt.imshow(im)
 
-plt.imshow(im)
+        # plt.figure()
+        # plt.imshow(cv2.cvtColor(light,cv2.COLOR_HSV2RGB))
 
-plt.figure()
-plt.imshow(cv2.cvtColor(light,cv2.COLOR_HSV2RGB))
+        # plt.show()
 
-plt.show()
+tl = tlDetector()
+rospy.init_node('light_node', anonymous=False)
+rospy.spin()
