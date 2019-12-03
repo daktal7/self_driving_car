4a21c636a805306769b4964fc776cf0197294b19
daktal7 <dakotaflanary@gmail.com>
2019-12-03 05:28:55 +0000

    foo

--- car_ros/src/drive_control/src/testIntersections.py
+++ car_ros/src/drive_control/src/testIntersections.py
@@ -9,18 +9,24 @@ import numpy as np
 #the function (copied from Learning Suite) to get you location 
 def getCoor(color):
     # api-endpoint
-    URL = "http://192.168.1.8:8080/%s" % color
+	URL = "http://192.168.1.8:8080/%s" % color
  
     # sending get request and saving the response as response object
-    r = requests.get(url = URL)
+	r = requests.get(url = URL)
  
     # extracting data
-    coorString = r.text
-    coordinates = coorString.split()
-    latitude = float(coordinates[0])
-    longitude = float(coordinates[1])
-    return (latitude, longitude)
-
+	coorString = r.text
+	coordinates = coorString.split()
+	try:
+		latitude = float(coordinates[0])
+		longitude = float(coordinates[1])
+		return (latitude, longitude)
+	except IndexError:
+		print("IndexError in accessing coordinates")
+		return(0,0)
+	except:
+		print("other errror")
+		return(0,0)
 
 
 
