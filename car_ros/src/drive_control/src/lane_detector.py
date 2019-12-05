53009a64164c59102cd7c0826afaea15563b19c7
zjwalker9 <zjwalker9@gmail.com>
2019-12-05 12:27:04 -0700

    a

--- car_ros/src/drive_control/src/lane_detector.py
+++ car_ros/src/drive_control/src/lane_detector.py
@@ -49,7 +49,7 @@ class image_displayer:
 			lines_to_average_right = np.array(lines_to_average_right)
 			lines_to_average_left = left_line
 			lines_to_average_left = np.array(lines_to_average_left)
-			left_offset = 155
+			left_offset = 165
 			right_offset = -225
 			offset = 0
 			# if left_line.shape[0] != 0 and right_line.shape[0] != 0: #this means I have a left and right line
