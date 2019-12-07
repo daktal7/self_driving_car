40e71e282e8ebc1b240de9a4c437a639bd0a487d
zjwalker9 <zjwalker9@gmail.com>
2019-12-07 12:27:10 -0700

    Update drive.py

--- car_ros/src/drive_control/src/drive.py
+++ car_ros/src/drive_control/src/drive.py
@@ -97,7 +97,7 @@ def turn_right_intersection():
     command = "!steering" + str(angle) + "\n"
     ser.write(command.encode())
     drive(STARTUP_SPEED)
-    straightTime = 2.5
+    straightTime = 2
     rightTime = 2.8
     for i in range(RES):
         if OBJECT_DETECTED:
