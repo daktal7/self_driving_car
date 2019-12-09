ecf8528fca0731fc4f784817b5fa582c71fcaa4c
jaceRozsa <jacerozsa@Jaces-MacBook-Pro.local>
2019-12-09 07:47:32 -0700

    foo

--- car_ros/src/drive_control/src/wpManager.py
+++ car_ros/src/drive_control/src/wpManager.py
@@ -30,7 +30,7 @@ WI_4 = np.array([[318,491],[462,635],[362,737],[190,585]])
 WI_5 = np.array([[300,140],[484,138],[490,2],[294,2]])
 WI_6 = np.array([[134,158],[278,148],[272,324],[134,332]])
 WI_7 = np.array([[782,1407],[938,1405],[940,1225],[788,1229]])
-WI_8 = np.array([[523,1427],[783,1423],[785,1578],[521,1578]])
+WI_8 = np.array([[570,1582],[574,1429],[777,1424],[780,1584]])
 WARNING_INTERSECTIONS = np.array([WI_1,WI_2,WI_3,WI_4,WI_5,WI_6,WI_7,WI_8])
 
 #Distance that the car must be from a waypoint in order bump that waypoint off our list.
